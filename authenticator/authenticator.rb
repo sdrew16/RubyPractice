f23764a135052a243390abe9f809c1780ea5ab94
Shannon Drew <shannon.nicole.drew@gmail.com>
2023-06-27 09:12:01 -0700

    usernames not case sensitive

--- authenticator/authenticator.rb
+++ authenticator/authenticator.rb
@@ -1,17 +1,17 @@
 # this is a basic idea of an authenticator (homework for The Complete Ruby on Rails Developer Course)
 
 users = [
-  {username: 'John', password: 'pass1'},
-  {username: 'Sally', password: 'pass2'},
-  {username: 'Mark', password: 'pass3'},
-  {username: 'Bob', password: 'pass4'},
-  {username: 'Amy', password: 'pass5'}
+  {username: 'John'.downcase, password: 'pass1'},
+  {username: 'Sally'.downcase, password: 'pass2'},
+  {username: 'Mark'.downcase, password: 'pass3'},
+  {username: 'Bob'.downcase, password: 'pass4'},
+  {username: 'Amy'.downcase, password: 'pass5'}
 ]
 
 def authenticate_user(known_users)
   for attempts in 0...3 
     p 'username: '
-    uname = gets.chomp
+    uname = gets.chomp.downcase
     p 'password: '
     pword = gets.chomp
 
